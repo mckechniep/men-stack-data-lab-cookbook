51662d1e8400adfa541d3b396908fe118d3edcbe
mckechniep@gmail.com <mckechniep@MagicalBox.>
2024-10-03 11:43:57 -0400

    recipe object error fixed

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const ingredientSchema = new mongoose.Schema({
-name: {
-type: String,
-required: true,
-}
+  name: { type: String, required: true },
 });
 
 const Ingredient = mongoose.model("Ingredient", ingredientSchema);
--- models/recipe.js
+++ models/recipe.js
@@ -1,22 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const recipeSchema = new mongoose.Schema({
-   name: {
-     type: String,
-     required: true,
-   },
-   instructions: {
-    type: String,
-  required: true,
-  },
-  owner: {
-    // type: because ObjectId is a type from Mongoose, not a type from JavaScript   
-     type: mongoose.Schema.Types.ObjectId, ref: "User",
-     required: true,
-  },
-  ingredients: {
-    type: mongoose.Schema.Types.ObjectId, ref: "Ingredient",
-     required: true,
-  },
- });
+  name: { type: String, required: true },
+  instructions: { type: String },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  ingredients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ingredient" }],
+});
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
